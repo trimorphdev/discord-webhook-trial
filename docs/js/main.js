054bbb98e9cdcfea8ae69347c0d58ffe563b76ef
Zack <trimorphyt@gmail.com>
2021-07-21 19:12:36 -0400

    Add webhook input

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,12 @@
 (function() {
-    const webhookUrl = 'https://discord.com/api/webhooks/867531006432772146/Auq2SVSWThLBXd4c8jKH4YLrmFRb3AGuwpCyRnv9ValFWtZRI5-JaIEgF6XxZnVy7mEt';
+    const defaultWebhook = 'https://discord.com/api/webhooks/867531006432772146/Auq2SVSWThLBXd4c8jKH4YLrmFRb3AGuwpCyRnv9ValFWtZRI5-JaIEgF6XxZnVy7mEt';
 
     /**
      * Sends an embed to the discord webhook.
      * @param {string} title the title of the embed.
      * @param {string} message the message of the embed.
      */
-    function sendEmbed(title, message) {
+    function sendEmbed(webhook, title, message) {
         let data = {
             embeds: [
                 {
@@ -17,22 +17,19 @@
         }
 
         let http = new XMLHttpRequest();
-        http.open('POST', webhookUrl);
-
-        http.addEventListener('load', (ev) => {
-            console.log(http.responseText);
-        });
-
+        http.open('POST', webhook);
         http.setRequestHeader('Content-Type', 'application/json');
         http.send(JSON.stringify(data));
-        console.log('Sent HTTP request!');
     }
 
     window.handleForm = function() {
-        console.log('hello world!')
+        let webhook = document.getElementById('webhook').value;
         let title = document.getElementById('title').value;
         let desc = document.getElementById('desc').value;
-        console.log(`Data: ${title}; ${desc}`)
-        sendEmbed(title, desc);
+
+        if (!webhook || webhook.trim() == '')
+            return sendEmbed(defaultWebhook, title, desc);
+
+        sendEmbed(webhook, title, desc);
     }
 })();
